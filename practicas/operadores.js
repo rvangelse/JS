9a1bc17ab00dae3deeced723e387dd5825bdb735
Angel Reyes V. <rvangelse@gmail.com>
2025-03-26 12:26:07 -0300

    Corrección

--- practicas/operadores.js
+++ practicas/operadores.js
@@ -3,7 +3,7 @@ array2 = [5, 2, "texto 2", 3];
 
 console.log(`¿El número 10 es mayor que el número 5? ${array1[0] > array2[0]}`);
 console.log(`¿El número 10 es menor que el número 5? ${array1[0] < array2[0]}`);
-console.log(`¿El número 1 es igual al número2? ${array1[1] == array2[1]}`);
+console.log(`¿El número 1 es igual al número 2? ${array1[1] == array2[1]}`);
 console.log(`¿El texto 1 es igual al texto 2? ${array1[2] == array2[2]}`);
 console.log(`¿Los objetos son iguales? ${array1 == array2}`);
 console.log(`AND lógico: ${(array1[0]>array2[0]) && (array1[0]<array2[0])}`);
